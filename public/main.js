04059628fc8b42657981ac13a63b479d371f5c75
Colin Aulds <caulds989@gmail.com>
2016-08-17 11:59:12 -0700

    re-commmit

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    //Here you create a Manager object by calling the io function.
+    //This object will automatically attempt to connect to the server,
+    //and will allow you to send and receive messages.
     var socket = io();
     var input = $('input');
     var messages = $('#messages');
@@ -14,6 +17,14 @@ $(document).ready(function() {
 
         var message = input.val();
         addMessage(message);
+        //Here we call the socket.emit function. This sends a message
+        //to the Socket.IO server. The first argument is a name for
+        //our message - in this case we simply call it message.
+        //The second argument is some data to attach to our message.
+        //In this case it's the contents of the text box.
+        socket.emit('message', message);
         input.val('');
     });
 });
+
+socket.on('message', addMessage);
--- server.js
+++ server.js
@@ -20,6 +20,15 @@ var io = socket_io(server);
 //This will be called whenever a new client connects to the Socket.IO server.
 io.on('connection', function (socket) {
     console.log('Client connected');
+
+    //Here you add a new listener to the socket which is used to communicate
+    //with the client. When a message with the name message is received on
+    //the socket you simply print out the message.
+    socket.on('message', function(message) {
+        console.log('Received message:', message);
+        //Finally you need to broadcast the message to any other clients who are connected.
+        socket.broadcast.emit('message', message);
+    });
 });
 
 // Finally notice that you now call server.listen rather than app.listen
