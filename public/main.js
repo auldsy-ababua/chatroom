2161d1703b6716832ad315c747b56eb4b121dd45
Colin Aulds <caulds989@gmail.com>
2016-08-18 14:31:56 -0700

    added num of users, and user names

--- public/main.js
+++ public/main.js
@@ -1,21 +1,35 @@
 $(document).ready(function() {
-    //Here you create a Manager object by calling the io function.
-    //This object will automatically attempt to connect to the server,
-    //and will allow you to send and receive messages.
-    var socket = io();
-    var input = $('input');
-    var messages = $('#messages');
+  //Here you create a Manager object by calling the io function.
+  //This object will automatically attempt to connect to the server,
+  //and will allow you to send and receive messages.
+  var socket = io();
+  var messages = $('#messages');
 
-    var addMessage = function(message) {
-        messages.append('<div>' + message + '</div>');
-    };
+  var addMessage = function(message) {
+      messages.append('<div>' + message + '</div>');
+  };
 
-    input.on('keydown', function(event) {
+  socket.on('totalUsers', function(message){
+    $('#count').text(message);
+    console.log(message);
+  });
+
+  $("#userName").on('keydown', function(event) {
+      if (event.keyCode == 13) {
+          $("#hide").hide();
+          // read the username from the input field
+          // send the username to the server
+          socket.emit('login', $("#userName").val());
+
+      }
+  });
+
+    $('#message').on('keydown', function(event) {
         if (event.keyCode != 13) {
             return;
         }
 
-        var message = input.val();
+        var message = $("#message").val();
         addMessage(message);
         //Here we call the socket.emit function. This sends a message
         //to the Socket.IO server. The first argument is a name for
@@ -23,8 +37,7 @@ $(document).ready(function() {
         //The second argument is some data to attach to our message.
         //In this case it's the contents of the text box.
         socket.emit('message', message);
-        input.val('');
+        $("#message").val('');
     });
+    socket.on('message', addMessage);
 });
-
-socket.on('message', addMessage);
--- server.js
+++ server.js
@@ -14,22 +14,38 @@ app.use(express.static('public'));
 //which is an EventEmitter
 var server = http.Server(app);
 var io = socket_io(server);
-
+//{ &*6876876876876: 'Mario', 9798lhlkjljlj: 'Colin'  }
+var allUsernames = {};
 
 //Next you add a listener to the connection event of the server.
 //This will be called whenever a new client connects to the Socket.IO server.
 io.on('connection', function (socket) {
-    console.log('Client connected');
+
 
     //Here you add a new listener to the socket which is used to communicate
     //with the client. When a message with the name message is received on
     //the socket you simply print out the message.
     socket.on('message', function(message) {
         console.log('Received message:', message);
+        var username = allUsernames[socket.id];
         //Finally you need to broadcast the message to any other clients who are connected.
-        socket.broadcast.emit('message', message);
+        socket.broadcast.emit('message', username + ": " + message);
     });
+
+    //when a new client provides a username, it is added to the users object.
+    socket.on('login', function(username) {
+        allUsernames[socket.id] = username;
+        socket.broadcast.emit('message', username);
+        socket.broadcast.emit('totalUsers', Object.keys(allUsernames).length);
+        socket.emit('totalUsers', Object.keys(allUsernames).length);
+    });
+    socket.on('disconnect',function(){
+        socket.broadcast.emit('message', 'Client Disonnected' + socket.id);
+        delete allUsernames[socket.id];
+        socket.broadcast.emit('totalUsers', Object.keys(allUsernames).length);
+    })
 });
+
 
 // Finally notice that you now call server.listen rather than app.listen
 server.listen(8080, () => console.log('server started'));
